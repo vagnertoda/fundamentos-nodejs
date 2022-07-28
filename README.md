eec31e27af7332ef2987fe72c371b09c25be34c8
Vagner E. Toda <vagner_ederli@adsp.matsuda.com.br>
2022-07-28 16:13:13 -0300

    second commit

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
 ### Comandos
-# yarn init -y
-# yarn add express
-# yarn add nodemon -D (-D significa que esta dependencia vai ficar como desenvolvimento)
-# adicionando o script no package.json
-# "scripts": {
-#  "dev": "nodemon src/index.js"
-# },
-
-# yarn dev
+- yarn init -y
+- yarn add express
+- yarn add nodemon -D (-D significa que esta dependencia vai ficar como desenvolvimento)
+- adicionando o script no package.json
+- "scripts": {
+-  "dev": "nodemon src/index.js"
+- },
+- yarn dev
 
 ### Tipo de parametros
 
-# Route Params -> Identificar um recurso editar/deletar/busca
-# Query Params -> Paginção/ Filtro
-# Body Params -> Os objetos inserção/alteracao (json) exemplo: enviar dados para cadastro
+- Route Params -> Identificar um recurso editar/deletar/busca
+- Query Params -> Paginação/ Filtro
+- Body Params -> Os objetos inserção/alteracao (json) exemplo: enviar dados para cadastro
+
+### Middlewares
+
+- São funções que ficam entre os request e response
+- Ex: usados para validar tokens
+- Ex: validar usuarios
 
 ### Exemplo
 ### Requisitos
 
 -[x] Deve ser possivel criar uma conta
 -[x] Deve ser possivel buscar o extrato bancario do cliente
--[] Deve ser possivel realizar um deposito
--[] Deve ser possivel realizar um saque
+-[x] Deve ser possivel realizar um deposito
+-[x] Deve ser possivel realizar um saque
 -[] Deve ser possivel buscar o extrato bancario do cliente por data
 -[] Deve ser possivel atualizar dados da conta do cliente
 -[] Deve ser possivel obter dados da conta do cliente
@@ -30,8 +35,8 @@
 ### Regras de negocio
 
 -[x] Não deve ser possivel cadastrar uma conta com CPF já existente
+-[x] Não deve ser possivel buscar extrato em uma conta não existente
 -[x] Não deve ser possivel fazer deposito em uma conta não existente
--[] Não deve ser possivel buscar extrato em uma conta não existente
--[] Não deve ser possivel fazer um saque em uma conta não existente
+-[x] Não deve ser possivel fazer um saque em uma conta não existente
+-[x] Não deve ser possivel fazer saque quando o saldo for insulficiente
 -[] Não deve ser possivel excluir um conta não existente
--[] Não deve ser possivel fazer saque quando o saldo for insulficiente
--- src/index.js
+++ src/index.js
@@ -13,6 +13,37 @@ const customers = []
 *id - uuid
 *statment []
 */
+
+//Middlewares - verificando se existe a conta com cpf
+function verifyIfExistsAccountCPF(request, response, next){
+    const { cpf } = request.headers;
+    //const { cpf } = request.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({"error": "Customer not found!"})
+    }
+    //passa para minha rota que contem
+    request.customer = customer;
+
+    return next();
+}
+
+//realiza operação de credito e debito da conta
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
+//criando a conta
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
    
@@ -36,16 +67,54 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response)=>{
-    const { cpf } = request.params;
+//app.use(verifyIfExistsAccountCPF); todas minhas rotas abaixo vai passar pelo middleware
+//pegando o statement da conta com cpf
+app.get("/statement/:cpf", verifyIfExistsAccountCPF, (request, response)=>{  
+    const { customer } = request;
+    return response.json(customer.statement);
+});
 
-    const customer = customers.find((customer) => customer.cpf === cpf);
+//efetuando deposito
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description , amount } = request.body;
+    const customer = request.customer;
 
-    if(!customer){
-        return response.status(400).json({"error": "Customer not found!"})
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+})
+
+//efetuando saque
+app.post("/withdraw", verifyIfExistsAccountCPF,(request, response)=> {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    //pegando o saldo existente
+    const balance = getBalance(customer.statement);
+    //validando se o saldo é menor que o valor de saque
+    if(balance < amount){
+        return response.status(400).json({error: "Insufficient funds!"});
     }
 
-    return response.json(customer.statement);
+    //passando objeto para const statementOperation
+    const statementOperation = {        
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+    //gravando em memoria 
+    customer.statement.push(statementOperation);
+
+    //se ok retorna status 201
+    return response.status(201).send();
+
 });
 
 app.listen(3333);
